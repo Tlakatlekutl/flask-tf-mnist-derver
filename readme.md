3ede7c3adde8d794dad445c88514b7af71432b7d
Tlakatlekutl <ary3004@yandex.ru>
2017-11-20 21:49:16 +0300

    init commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Flask tensorflow mnist
+## created for mppr lab3
+
+Run code by this command:
+```sh
+$ FLASK_APP=app.py flask run
+```
+Verify what Flask already inslalled
+
+For debugging logs and autocodereload you can use 'Debug mode':
+```sh
+$ FLASK_APP=app.py FLASK_DEBUG=1 flask run
+```
---
+++ train.py
@@ -0,0 +1,50 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+import argparse
+import sys
+import numpy as np
+
+FLAGS = None
+
+
+def main(_):
+    # Import data
+    mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+
+    # Create the model
+    x = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+    y = tf.matmul(x, W) + b
+
+    # Define loss and optimizer
+    y_ = tf.placeholder(tf.float32, [None, 10])
+
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    # Train
+    for _ in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    # Test trained model
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images,
+                                        y_: mnist.test.labels}))
+
+    saver = tf.train.Saver()
+    save_path = saver.save(sess, './models/mnist.ckpt')
+    print("Model saved in file: %s" % save_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='MNIST_data/',
+                        help='Directory for storing input data')
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
